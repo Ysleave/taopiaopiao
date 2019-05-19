e596a3761a4b8632ee30e524089d0c43888d50a6
Administrator <1397217760@qq.com>
2019-05-19 22:33:10 +0800

    add setData

---
+++ src/oaxios.js
@@ -0,0 +1,11 @@
+// import axios from 'axios'
+// axios.defaults.baseURL = 'http://39.97.33.178';
+// axios.interceptors.request.use(function (config) {
+//     config.params = {
+//         ...config.params,
+//     }
+//     return config;
+// },function (error){
+//     return Promise.reject(error);
+// });
+// export default axios;
