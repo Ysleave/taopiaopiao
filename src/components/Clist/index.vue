27bf5552e832c585f43a50e724062f1f36c5058c
Administrator <1397217760@qq.com>
2019-05-20 19:06:47 +0800

    add getcity

--- src/components/Clist/index.vue
+++ src/components/Clist/index.vue
@@ -1,21 +1,24 @@
 <template>
-<div class="cinema_body">
-				<ul>
-					<li v-for="item in cinemaList" :key="item.id">
-						<div>
-							<span>{{ item.nm }}</span>
-							<span class="q"><span class="price">{{ item.sellPrice}}</span> 元起</span>
-						</div>
-						<div class="address">
-							<span>{{ item.addr}}</span>
-							<span>{{ item.distance}}</span>
-						</div>
-						<div class="card">
-                		    <div v-for="(num,key) in item.tag" v-if="num === 1" :class="key | classCard" :key="key">{{ key | formatCard }}</div>
-       					</div>
-					</li>
-				</ul>
-			</div>
+  <div class="cinema_body">
+		<Loading v-if="isLoading"/>
+	<Scroller v-else>
+		<ul>
+			<li v-for="item in cinemaList" :key="item.id">
+				<div>
+					<span>{{ item.nm }}</span>
+					<span class="q"><span class="price">{{ item.sellPrice}}</span> 元起</span>
+				</div>
+				<div class="address">
+					<span>{{ item.addr}}</span>
+					<span>{{ item.distance}}</span>
+				</div>
+				<div class="card">
+					<div v-for="(num,key) in item.tag" v-if="num === 1" :class="key | classCard" :key="key">{{ key | formatCard }}</div>
+				</div>
+			</li>
+		</ul>
+	</Scroller>
+  </div>
 </template>
 
 <script>
@@ -24,14 +27,21 @@ export default {
 
   data () {
     return {
-		cinemaList:[]
+		cinemaList:[],
+		isLoading:true,
+		prevCityId : -1
     }
   },
-  mounted(){
-	  this.axios.get('/api/cinemaList?cityId=10').then(res=>{
+  activated(){
+		 var cityId = this.$store.state.city.id;
+     if( this.prevCityId === cityId ) {return}
+     this.isLoading = true;
+	  this.axios.get('/api/cinemaList?cityId='+ cityId).then(res=>{
 		  var msg = res.data.msg;
 		  if(msg==='ok'){
-              this.cinemaList = res.data.data.cinemas;
+							this.cinemaList = res.data.data.cinemas;
+							this.isLoading =false;
+							this.prevCityId = cityId
 		  }
 	  })
   },
---
+++ src/components/JS/Message/index.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="Message">
+    <h2>{{ title}}</h2>
+    <p>{{ content }}</p>
+    <div>
+        <div @touchstart="handleCancel">{{ cancel }}</div>
+        <div @touchstart="handleok">{{ ok }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Message',
+
+  data () {
+    return {
+    }
+  },
+
+  methods: {}
+}
+</script>
+
+<style scoped>
+  .Message{
+      width: 200px;
+      height:120px;
+      border: 1px solid #ccc;
+      border-radius: 4px;
+      background: white;
+      box-shadow: 3px 3px 3px 3px #ccc;
+      position: absolute;
+      left: 50%;
+      top: 50%;
+      margin: -60px 0 0 -100px;
+  }
+  .Message h2{
+      text-align: center;
+      line-height: 40px;
+      font-size: 18px
+  }
+  .Message p{
+      text-align: center;
+      line-height: 40px;
+  }
+  .Message > div{
+      display: flex;
+      position: absolute;
+      bottom: 0;
+      width: 100%;
+      border-top: 1px solid #ccc;
+  }
+  .Message > div div{
+      flex: 1;
+      text-align: center;
+      line-height: 30px;
+      border: 1px solid #ccc;      
+}
+/* .Message >div div:last-child{
+    border: none;
+} */
+
+</style>
