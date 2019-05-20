0dc6970141384d1fd62635b716bbb03249e832b9
Administrator <1397217760@qq.com>
2019-05-20 19:18:57 +0800

    new getcity

--- src/components/TabBar/index.vue
+++ src/components/TabBar/index.vue
@@ -5,7 +5,7 @@
 				<i class="iconfont icon-dianying"></i>
 				<p>电影</p>
 			</router-link>
-			<router-link tag="li" to="cinema">
+			<router-link tag="li" to="/cinema">
 				<i class="iconfont icon-yingyuan"></i>
 				<p>影院</p>
 			</router-link >
@@ -35,6 +35,7 @@ export default {
 #footer ul{ display: flex; text-align: center; height:50px; align-items:center;}
 #footer ul li{ flex:1; height:40px;}
 #footer ul li.active{ color: #f03d37;}
+#footer ul li.router-link-active{color: #f03d37;}
 #footer ul i{ font-size: 20px;}
 #footer ul p{ font-size: 12px; line-height: 18px;}
 </style>
